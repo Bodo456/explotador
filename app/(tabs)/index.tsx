108753e353bc626df84d25cca81532242cdf5c2e
Bodo456 <bodoscargo@gmail.com>
2026-05-08 21:34:03 +0200

    added a login-/register-field and changed the position and size of title

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,66 @@
-import { StyleSheet, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 export default function Index() {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+
+      <View style={styles.titleContainer}>
+      {/* title */}
+        <Text style={styles.title}>Explotador</Text>
+      </View>
+      {/* login-/register-field */}
+      <View style={styles.buttonContainer}>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>Login</Text>
+        </Pressable>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>Register</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#2b2b2b',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 80,
   },
+  titleContainer: {
+    position: 'absolute',
+    top: 130,
+  },
+  title: {
+    color: 'white',
+    fontSize: 50,
+    fontWeight: 'bold',
+  },
+  buttonContainer: {
+    width: '85%',
+    alignItems: 'center',
+    gap: 15,
+    borderColor: '#555',
+    borderWidth: 1,
+    backgroundColor: '#3a3a3a',
+    padding: 25,
+    borderRadius: 20,
+  },
+  button: {
+    backgroundColor: '#6a6a6a',
+    paddingVertical: 12,
+    paddingHorizontal: 40,
+    borderRadius: 10,
+    width: '70%',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: '#888',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 600,
+  }
 });
